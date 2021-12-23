a33fa91539cf5adae1f60c117d90cefe2cc3a089
Tigran <tigran.arsenyan.2018@gmail.com>
2021-12-24 00:23:04 +0400

    Initial Commit

---
+++ teleprom-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teleprom-ios
+//
+//  Created by Tigran Arsenyan on 12/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
